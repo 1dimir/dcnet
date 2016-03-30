cac08cbeabe048fa68c51dc7c15b63fe1ef27456
Dmitry Sorokin <inbox@dimir.name>
2016-03-31 00:42:36 +0300

    add setup script, correct lisence

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+from distutils.core import setup
+
+setup(
+        name='dcnet',
+        version='0.1',
+        description='Danceconvention.net api python implementation',
+        author='Dmitry Sorokin',
+        author_email='inbox@dimir.name',
+        lincence='MIT',
+        py_modules='dcnet'
+)
+
